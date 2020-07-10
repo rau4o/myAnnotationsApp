efa2ec8655ae86f72fb581e4952145b7c629ab5c
Rauan <rkoshkinbayev@gmail.com>
2020-07-10 22:43:04 +0600

    Initial Commit

---
+++ MyAnnotations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyAnnotations
+//
+//  Created by rau4o on 7/10/20.
+//  Copyright © 2020 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
