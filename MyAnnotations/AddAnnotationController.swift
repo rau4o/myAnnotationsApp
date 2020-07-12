7b9369f05300b8a15ef7242081eafef9f40a0f33
Rauan <rkoshkinbayev@gmail.com>
2020-07-12 19:45:48 +0600

    first commit

---
+++ MyAnnotations/AddAnnotationController.swift
@@ -0,0 +1,30 @@
+//
+//  AddAnnotationController.swift
+//  MyAnnotations
+//
+//  Created by rau4o on 7/12/20.
+//  Copyright © 2020 rau4o. All rights reserved.
+//
+
+import UIKit
+
+class AddAnnotationController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
